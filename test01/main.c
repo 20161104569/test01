42506a31a33012ff408064138c8ee0e7dbe62198
20161104569 <2174037646@qq.com>
2017-06-19 11:08:05 +0800

    累加
    累加

--- test01/main.c
+++ test01/main.c
@@ -13,11 +13,12 @@ int main()
     int i,sum;
     sum=0;
     i=1;
-    while(i<=100)
+    do
     {
         sum=sum+i;
         i=i+1;
-    }
+    }while(i<=100);
     printf("1+2+3+....+100=%d",sum);
     return 0;
 }
+    
